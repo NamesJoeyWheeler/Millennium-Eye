d70199d60879f2b3c700bfdd0be673bc2629c42d
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2021-08-28 22:46:03 +1000

    Update milleniumeye.py

--- milleniumeye.py
+++ milleniumeye.py
@@ -3,6 +3,4 @@ print("Developed By NamesJoeyWheeler")
 print("")
 var = input("Enter Stan Episode/Movie ID: ")
 print("---------------------------------")
-print("https://v12.cat.api.stan.com.au/programs/" + str(var))
-print(".json")
-
+print("https://api.stan.com.au/cat/v12/programs/" + str(var) + ".json")
